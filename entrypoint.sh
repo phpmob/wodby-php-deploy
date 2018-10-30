a17e8cb86dae528cda6e22d8b59a16d229956c8b
liverbool <nukboon@gmail.com>
2018-10-30 17:37:00 +0700

    add PHP_INFO

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # deploy
 if [ "${REPOSITORY}" == "" ]; then
   echo 'WARN: There are no `REPOSITORY` env defined.';
+
+  if [ "${PHP_INFO}" == "1" ]; then
+    mkdir -p "${APP_ROOT}/current/public"
+    echo "<?php phpinfo();" > "${APP_ROOT}/current/public/index.php"
+  fi
 else
   # if not symlink remove it!
   if [ ! -L "${APP_ROOT}/current" ] && [ -d "${APP_ROOT}/current" ]; then
