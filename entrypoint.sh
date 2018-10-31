085debc20d662495f85a15dc1315be95d85e3ddf
liverbool <nukboon@gmail.com>
2018-10-31 17:46:00 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # deploy
-if [ "${REPOSITORY}" == "" ]; then
-  echo 'WARN: There are no `REPOSITORY` env defined.';
+if [ "${DEPLOY_REPOSITORY}" == "" ]; then
+  echo 'WARN: There are no `DEPLOY_REPOSITORY` env defined.';
 
-  if [ "${PHP_INFO}" == "1" ]; then
+  if [ "${DEPLOY_PHP_INFO}" == "1" ]; then
     mkdir -p "${APP_ROOT}/current/public"
     echo "<?php phpinfo();" > "${APP_ROOT}/current/public/index.php"
   fi
@@ -15,11 +15,11 @@ else
     rm -rf "${APP_ROOT}/current"
   fi
 
-  if [ "${DEPLOYER_TASK}" == "" ]; then
-    DEPLOYER_TASK=deploy
+  if [ "${DEPLOY_DEPLOYER_TASK}" == "" ]; then
+    DEPLOY_DEPLOYER_TASK=deploy
   fi
 
-  /var/vendor/bin/dep -vvv --file=/deploy.php $DEPLOYER_TASK
+  /var/vendor/bin/dep -vvv --file=/deploy.php $DEPLOY_DEPLOYER_TASK
   # chown -R www-data:www-data $APP_ROOT
 fi
 
